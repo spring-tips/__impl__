3a317c2608b27f98612a32e8aa9fa9eb4a4a09c9
Josh Long <starbuxman@gmail.com>
2018-03-11 16:44:01 -0400

    repos

--- github/repos.rb
+++ github/repos.rb
@@ -1,12 +1,17 @@
 #!/usr/bin/env ruby
 
 if __FILE__ == $0
-  root = $1 ? $1 : "#{ENV['HOME']}/Desktop/st-repo/repos.txt"
-  File.new(root)
+
+  expand_dir = File.expand_path('../../..', $0)
+
+  repository_file = File.join(File.expand_path('..', $0), 'repos.txt')
+
+  File.new(repository_file)
       .readlines
-      .map {|line| line.strip}
-      .select {|x| not Dir.exists?(x)}
-      .map {|line| "git@github.com:spring-tips/#{line}.git"}
-      .map {|x| "git clone #{ x }"}
+      .map {|x| x.strip}
+      .map {|line| {:repo => line, :dir => File.join(expand_dir, line)}}
+      .select {|x| not Dir.exists?(x[:dir])}
+      .map {|line| {:repo => "git@github.com:spring-tips/#{ line[:repo] }.git", :dir => line[:dir]}}
+      .map {|x| "git clone #{ x[:repo] } #{ x[:dir] } "}
       .each {|x| `#{x}`}
 end
