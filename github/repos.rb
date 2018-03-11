3c1084946d385392d6ff5b0484d279d71b6663a1
Josh Long <starbuxman@gmail.com>
2018-03-11 16:45:49 -0400

    1.9 syntax

--- github/repos.rb
+++ github/repos.rb
@@ -1,17 +1,14 @@
 #!/usr/bin/env ruby
 
-if __FILE__ == $0
-
-  expand_dir = File.expand_path('../../..', $0)
-
-  repository_file = File.join(File.expand_path('..', $0), 'repos.txt')
-
+if $PROGRAM_NAME == __FILE__
+  expand_dir = File.expand_path('../../..', $PROGRAM_NAME)
+  repository_file = File.join(File.expand_path('..', $PROGRAM_NAME), 'repos.txt')
   File.new(repository_file)
       .readlines
-      .map {|x| x.strip}
-      .map {|line| {:repo => line, :dir => File.join(expand_dir, line)}}
-      .select {|x| not Dir.exists?(x[:dir])}
-      .map {|line| {:repo => "git@github.com:spring-tips/#{ line[:repo] }.git", :dir => line[:dir]}}
-      .map {|x| "git clone #{ x[:repo] } #{ x[:dir] } "}
+      .map(&:strip)
+      .map {|line| {repo: line, dir: File.join(expand_dir, line)}}
+      .reject {|x| Dir.exist?(x[:dir])}
+      .map {|line| {repo: "git@github.com:spring-tips/#{line[:repo]}.git", dir: line[:dir]}}
+      .map {|x| "git clone #{x[:repo]} #{x[:dir]} "}
       .each {|x| `#{x}`}
 end
