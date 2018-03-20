2f306c1608f256b9329e62c8088f226f66343751
Josh Long <starbuxman@gmail.com>
2018-03-19 22:47:02 -0700

    ups

--- github/install.sh
+++ github/install.sh
@@ -3,11 +3,9 @@
 bootstrap=_impl_
 st_dir=${ST_CODE:-${HOME}/spring-tips-code-test} # set $ST_CODE to control where this is
 export ST_CODE=$st_dir
-
 mkdir -p $st_dir
 full_bootstrap=$st_dir/$bootstrap
 [[ -e $full_bootstrap ]] || git clone git@github.com:spring-tips/_impl_.git $full_bootstrap
-echo $full_bootstrap/..
 cd $full_bootstrap/github/
 ls
 
