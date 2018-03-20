0277c59628ece3045b73800b4678166ac75954b2
Josh Long <starbuxman@gmail.com>
2018-03-19 22:21:30 -0700

    i

--- github/installer/install.sh
+++ github/installer/install.sh
@@ -6,5 +6,7 @@ mkdir -p $HOME/$st_dir/
 full_bootstrap=$HOME/$st_dir/$bootstrap
 echo $full_bootstrap
 [[ -e $full_bootstrap ]] && echo "directory exists." || echo "no directory."
+
 exit
-git clone git@github.com:spring-tips/_impl_.git
+git clone git@github.com:spring-tips/_impl_.git $full_bootstrap
+cd
