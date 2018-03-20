58ba3b4fd499a53b0764bbfd80b15cbbeb3ed709
Josh Long <starbuxman@gmail.com>
2018-03-19 23:20:34 -0700

    upd

--- github/install.sh
+++ github/install.sh
@@ -2,8 +2,8 @@
 
 bootstrap=_impl_
 st_dir=${ST_CODE:-${HOME}/spring-tips-code-test}
-export ST_CODE=$st_dir
 mkdir -p $st_dir
 full_bootstrap=$st_dir/$bootstrap
+echo $full_bootstrap
 [[ -e $full_bootstrap ]] || git clone git@github.com:spring-tips/_impl_.git $full_bootstrap
 $full_bootstrap/github/repos.rb
--- github/repos.rb
+++ github/repos.rb
@@ -1,21 +1,21 @@
 #!/usr/bin/env ruby
 
-puts "trying.."
+
 st=ENV["ST_HOME"]
-puts st
 
-if false
+expand_dir = st
+repository_file = File.join(File.join(expand_dir, '_impl_/github'), 'repos.txt')
 
-  expand_dir = File.expand_path('../../..', $PROGRAM_NAME)
-  repository_file = File.join(File.expand_path('..', $PROGRAM_NAME), 'repos.txt')
-  File.new(repository_file)
-      .readlines
-      .map(&:strip)
-      .map {|line| {repo: line, dir: File.join(expand_dir, line)}}
-      .reject {|x| Dir.exist?(x[:dir])}
-      .map {|line| {repo: "git@github.com:spring-tips/#{line[:repo]}.git", dir: line[:dir]}}
-      .map {|x| "git clone #{x[:repo]} #{x[:dir]} "}
-      .each {|x| `#{x}`}
+puts expand_dir
+puts repository_file
 
+exit (1)
 
-end
+File.new(repository_file)
+    .readlines
+    .map(&:strip)
+    .map {|line| {repo: line, dir: File.join(expand_dir, line)}}
+    .reject {|x| Dir.exist?(x[:dir])}
+    .map {|line| {repo: "git@github.com:spring-tips/#{line[:repo]}.git", dir: line[:dir]}}
+    .map {|x| "git clone #{x[:repo]} #{x[:dir]} "}
+    .each {|x| `#{x}`}
